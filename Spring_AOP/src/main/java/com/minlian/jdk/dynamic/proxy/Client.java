5f20db7fa66573bfa1501d1616d8a065216bd80e
minlian <minlian666@163.com>
2019-12-18 01:07:13 +0800

    learing dynamic proxy

---
+++ Spring_AOP/src/main/java/com/minlian/jdk/dynamic/proxy/Client.java
@@ -0,0 +1,12 @@
+package com.minlian.jdk.dynamic.proxy;
+
+public class Client {
+    public static void main(String[] args) {
+        // 保存生成的代理类的字节码文件
+        System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
+
+        // jdk动态代理测试
+        Subject subject = new JDKDynamicProxy(new RealSubject()).getProxy();
+        subject.doSomething();
+    }
+}
---
+++ Spring_AOP/src/main/java/com/minlian/jdk/dynamic/proxy/Subject.java
@@ -0,0 +1,5 @@
+package com.minlian.jdk.dynamic.proxy;
+
+public interface Subject {
+    void doSomething();
+}
